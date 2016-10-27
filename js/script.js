e5fa940359af22bad9a394ed5333c95bc1b4cb4b
Erica Nafziger and Eric Raetz <eric@>
2016-10-27 13:43:36 -0700

    add function to verify ones place

--- js/script.js
+++ js/script.js
@@ -1,25 +1,59 @@
+var romanNumeral = function(input) {
 
-// var numbers = {M:1000, D:500, C:100, L:50, X:10, V:5, I:1};
-// 	var romanNumeral = function(input) {
-// 		for (input in number) {
-// 			alert(number);
-// 		}
-//
-//
-//
-// 	} //end of function
-var array = [];
-var romanNumeral = function(number) {
-	for (var i = 0; i < number; i++) {
-		array.push("I");
+	// var numbers = [1, 5, 10, 50, 100, 500, 1000];
+	// var numerals = ["I", "V", "X", "L", "C", "D", "M"];
+	var ones = ["","1","2", "3", "4", "5", "6", "7", "8", "9"];
+	var onesNumerals = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+	var tens = ["",10,20, 30, 40, 50, 60, 70, 80, 90];
+	var tensNumerals = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+	var hundreds = ["",10,20, 30, 40, 50, 60, 70, 80, 90];
+	var hundredsNumerals = ["", "I", "II", "III", "IV", "VI", "VII", "VIII", "IX"];
+
+	var array = input.split("");
+	var symbol;
+
+alert(array);
+	if (array.length === 1) {
+		for (var i = 1; i < ones.length; i++) {
+			if (ones[i] === array[0]) {
+				// debugge2r;
+				symbol = onesNumerals[i]
+			}
+	}
+	return symbol;
 
 }
-var join = array.join("");
-return join;
+		//end of for length 1 loop
+	// } else if (otherNums.indexOf(input) > -1) {
+	// 	for (var i = 0; i < otherNums.length; i++) {
+	// 		if (otherNums[i] === input) {
+	// 			var symbol = otherNumers[i]
+	// 			return symbol;
+	// 		}
+	// 	}//end of for 2 loop
+	// } else {
+	// 	return ("enter a valid number");
+	// }
 
-}
 
 
+} //end of function
+
+// var array = [];
+// var romanNumeral = function(number) {
+// 	for (var i = 0; i < number; i++) {
+// 		array.push("I");
+//
+// }
+// var join = array.join("");
+// return join;
+//
+// }
+//  if (number % 10 = 4) {
+//  array.push("IV")
+//  } elseif (number % 5 = 0 && number % 10 != 0) {
+//  array.push("V")
+// } elseif ...
 
 
 
@@ -29,7 +63,7 @@ return join;
 $(document).ready(function() {
 
 		$("form").submit(function(event) {
-			var number = Math.round($("input#number").val());
+			var number = $("input#number").val();
 			var result = romanNumeral(number);
 			$("#output").text(result);
 			event.preventDefault();
