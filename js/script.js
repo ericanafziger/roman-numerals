7ebafaf9c1d00114e815338e35ff9b5c4b438673
Erica Nafziger and Eric Raetz <erica@>
2016-10-27 09:59:57 -0700

    add all ones place numbers to function

--- js/script.js
+++ js/script.js
@@ -1,16 +1,39 @@
+
 var numbers = [1, 5, 10, 50, 100, 500, 1000];
 var numerals = ["I", "V", "X", "L", "C", "D", "M"];
-
+var otherNums = [2, 3, 4, 6, 7, 8, 9];
+var otherNumers = ["II", "III", "IV", "VI", "VII", "VIII", "IX"];
 // for (var i = 0; i < numbers.length; i++) {
-// 	numbers[i]
-// 	return
+// 	if (numbers[i] === input) {
+// 		var newNumber = numerals[i]
+// 		alert(newNumber);
+// 	}	else {
+// 	}
 // }
 
 
 var romanNumeral = function(input){
-	return false;
+	if (numbers.indexOf(input) > -1) {
+		for (var i = 0; i < numbers.length; i++) {
+			if (numbers[i] === input) {
+				var symbol = numerals[i]
+				return symbol;
+			}
+		}//end of for 1 loop
+	} else if (otherNums.indexOf(input) > -1) {
+		for (var i = 0; i < otherNums.length; i++) {
+			if (otherNums[i] === input) {
+				var symbol = otherNumers[i]
+				return symbol;
+			}
+		}//end of for 2 loop
+	} else {
+		return ("enter a valid number");
+	}
 
-}
+
+} //end of function
+
 
 
 
@@ -18,7 +41,6 @@ $(document).ready(function() {
 
 		$("form").submit(function(event) {
 			var number = Math.round($("input#number").val());
-			alert (number);
 			var result = romanNumeral(number);
 			$("#output").text(result);
 			event.preventDefault();
